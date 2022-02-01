56c82820bbf4caf249afe9c5bc6f74f0c208573d
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-01 08:37:54 +0100

    mise à jour du gitignore et debut de la classe Utilisateur (pour essai commit et push)

---
+++ src/main/java/fr/eni/projetEnchere/bo/Utilisateur.java
@@ -0,0 +1,14 @@
+package fr.eni.projetEnchere.bo;
+
+public class Utilisateur {
+	
+	private int noUtilisateur;
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+}
