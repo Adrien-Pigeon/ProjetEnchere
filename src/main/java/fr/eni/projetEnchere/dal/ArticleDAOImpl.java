a081cd821dfd0cfb6401dae8044fc0ae21f87f55
abru2021 <abru2021@177SE44-J0JBC5J.ad.campus-eni.fr>
2022-02-01 16:52:24 +0100

    add article

--- src/main/java/fr/eni/projetEnchere/dal/ArticleDAOImpl.java
+++ src/main/java/fr/eni/projetEnchere/dal/ArticleDAOImpl.java
@@ -1,8 +1,10 @@
 package fr.eni.projetEnchere.dal;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.projetEnchere.bo.ArticleVendu;
@@ -10,16 +12,26 @@ import fr.eni.projetEnchere.dal.Exception.DalException;
 
 public class ArticleDAOImpl implements ArticleVenduDAO{
 	
-	private static final String SELECT_ALL = "SELECT no_article, nom_article,pseudo,prix_vente,date_fin_encheres"
+	private static final String SELECT_ALL = "SELECT nom_article,description,prix_vente,,date_debut_encheres,date_fin_encheres"
+			+ "no_categorie,pseudo"
 			+ "FROM ARTICLES_VENDUS as a JOIN UTILISATEURS as u on(a.no_utilisateur = u.no_utilisateur)";
 
 	@Override
 	public void selectAll() throws DalException {
 		Connection cnx = null;
 		Statement stmt = null;
+		List<ArticleVendu> articles = new ArrayList<>();
+		
 		
 		try {
 			cnx = ConnectionProvider.getConnection();
+			stmt = cnx.createStatement();
+			ResultSet rs = stmt.executeQuery(SELECT_ALL);
+			ArticleVendu article;
+			while(rs.next()) {
+				;
+			}
+			
 		} catch (SQLException e) {
 			throw new DalException("Probleme sur la couche dal", e);
 		}
