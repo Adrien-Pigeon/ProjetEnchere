62fef081a28eae7d26c319202bfeb61e8003e20d
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-01 08:47:29 +0100

    mise a jour class "Utilisateur"

--- src/main/java/fr/eni/projetEnchere/bo/Utilisateur.java
+++ src/main/java/fr/eni/projetEnchere/bo/Utilisateur.java
@@ -3,6 +3,31 @@ package fr.eni.projetEnchere.bo;
 public class Utilisateur {
 	
 	private int noUtilisateur;
+	private String nom,prenom,email,telephone,rue,codePostal,ville,motDePasse;
+	private int credit;
+	private boolean administrateur;
+	
+	
+	
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(int noUtilisateur, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
 
 	public int getNoUtilisateur() {
 		return noUtilisateur;
@@ -11,4 +36,85 @@ public class Utilisateur {
 	public void setNoUtilisateur(int noUtilisateur) {
 		this.noUtilisateur = noUtilisateur;
 	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+	
 }
