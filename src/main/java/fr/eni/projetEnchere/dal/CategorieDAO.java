75e5cc42ceca1555e6052efea161934c153e4260
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-01 14:49:09 +0100

    MAJ ArticleVendu création CategorieDAO et DAOImpl Fix CategorieDAOImpl

---
+++ src/main/java/fr/eni/projetEnchere/dal/CategorieDAO.java
@@ -0,0 +1,16 @@
+package fr.eni.projetEnchere.dal;
+
+import java.util.List;
+
+
+import fr.eni.projetEnchere.bo.ArticleVendu;
+import fr.eni.projetEnchere.bo.Categorie;
+
+public interface CategorieDAO {
+
+	public List<ArticleVendu> selectAll();	
+	public void insert(Categorie categorie);
+	void insert(ArticleVendu articles);
+	
+	
+}
