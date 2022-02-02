1418428aa9bd7e463f922bfe1e024d030a556fd1
abru2021 <abru2021@177SE44-J0JBC5J.ad.campus-eni.fr>
2022-02-02 11:08:55 +0100

    change xml

--- src/main/java/fr/eni/projetEnchere/servlets/CreerUnCompte.java
+++ src/main/java/fr/eni/projetEnchere/servlets/CreerUnCompte.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.projetEnchere.bll.UtilisateurManager;
+import fr.eni.projetEnchere.helpers.HashPassword;
+
 /**
  * Servlet implementation class CreerUnCompte
  */
@@ -18,15 +21,21 @@ public class CreerUnCompte extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	this.getServletContext().getRequestDispatcher("/WEB-INF/jsp/Profil.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/jsp/Profil.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username=request.getParameter("username");
+		String prenom=request.getParameter("prenom");
+		String nom=request.getParameter("nom");
+		String password=HashPassword.hashpassword(request.getParameter("password"));
 		
+		UtilisateurManager um = UtilisateurManager.getInstance();
+		um.addUser(prenom, nom, username, password);
+		response.sendRedirect(request.getContextPath()+"/login");
 	}
 
 }
