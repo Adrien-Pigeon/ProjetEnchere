35cc4146ca5457ecfb04d7dba30a591aeddbdbb8
kelazhar2021 <kelazhar2021@SE46-33JWT72.ad.campus-eni.fr>
2022-02-03 16:09:13 +0100

    update session ModifierProfil & VendArticle

--- src/main/java/fr/eni/projetEnchere/servlets/ModifierProfil.java
+++ src/main/java/fr/eni/projetEnchere/servlets/ModifierProfil.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ModifierProfil
@@ -27,8 +28,23 @@ public class ModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/jsp/ModifierProfil.jsp").forward(request, response);
+		if (request.getSession() == null) {
+			// Recupere la session
+			
+			System.out.println("valided session");
+			// request.getRequestDispatcher("/AccueilConnecter?get=1").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/jsp/ModifierProfil.jsp").forward(request, response);			
+			
+			
+		}else {
+			
+			// Recupere la session
+			HttpSession session = request.getSession(); 
+			session.invalidate();
+			System.out.println("invalided  session");
+			 request.getRequestDispatcher("/WEB-INF/jsp/AccueilNonConnecter.jsp").forward(request, response);
+		}
+		
 	}
 
 	/**
--- src/main/java/fr/eni/projetEnchere/servlets/VendArticleServlet.java
+++ src/main/java/fr/eni/projetEnchere/servlets/VendArticleServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class VendArticle
@@ -26,8 +27,22 @@ public class VendArticleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/jsp/VendreArticle.jsp").forward(request, response);
+		if (request.getSession() == null) {
+			// Recupere la session
+			
+			System.out.println("valided  session");
+
+			// request.getRequestDispatcher("/AccueilConnecter?get=1").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/jsp/VendreArticle.jsp").forward(request, response);			System.out.println("valided session");
+			
+		}else {
+			
+			// Recupere la session
+			HttpSession session = request.getSession(); 
+			session.invalidate();
+			System.out.println("invalided  session");
+			 request.getRequestDispatcher("/WEB-INF/jsp/AccueilNonConnecter.jsp").forward(request, response);
+		}		
 	}
 
 	/**
