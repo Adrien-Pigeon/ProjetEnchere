4611ce90a821f22e4d18c669d654baca5a2bf25a
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-03 08:53:27 +0100

    Change CategorieDAO

--- src/main/java/fr/eni/projetEnchere/dal/CategorieDAO.java
+++ src/main/java/fr/eni/projetEnchere/dal/CategorieDAO.java
@@ -1,6 +1,7 @@
 package fr.eni.projetEnchere.dal;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,20 +13,19 @@ import fr.eni.projetEnchere.helpers.DBConnectPool;
 
 public class CategorieDAO {
 
-	private final static String LISTER ="SELECT *FROM CATEGORIES;";
-	private final static String INSERER ="INSERT INTO CATEGORIES (libelle) values (?);";
-	// private final static String SUPPRIMER = "delete from CATEGORIES where libelle =?;";
+	private final static String LISTER = "SELECT *FROM CATEGORIES;";
+	private final static String INSERER = "INSERT INTO CATEGORIES (libelle) values (?);";
+	// private final static String SUPPRIMER = "delete from CATEGORIES where libelle
+	// =?;";
 	// private final static String MODIFIER = "update CATEGORIES set libelle = ?";
 	private final static String SELECTBY = "select no_categorie, libelle from CATEGORIES where libelle = ?;";
 	private final static String SELECTBYID = "select no_categorie, libelle from CATEGORIES where no_categorie = ?;";
 
-
-	public static ArrayList<Categorie> lister() throws DalException{
+	public static ArrayList<Categorie> lister() throws DalException {
 		Connection cnx = null;
 		Statement stmt = null;
-		ResultSet rs =null;
-		
-		
+		ResultSet rs = null;
+
 		ArrayList<Categorie> listeCategorie = new ArrayList<>();
 		try {
 			cnx = DBConnectPool.seConnecter();
@@ -37,15 +37,122 @@ public class CategorieDAO {
 				categorie.setNoCategorie(rs.getInt("no_categorie"));
 				categorie.getLibelle();
 			}
-			
-		
-	} catch (SQLException) {
-		
-	} finally {
-		
+
+		} catch (SQLException e) {
+			throw new DalException("Problème - listeCategorie-" + e.getMessage());
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (cnx != null) {
+					cnx.close();
+				}
+
+			} catch (SQLException e) {
+				throw new DalException("Probleme - FermerConnexion" + e.getMessage());
+			}
+		}
+		return listeCategorie;
 	}
-	
 
+	public static void ajouter(String libelle) throws DalException {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
 
+		try {
+			cnx = DBConnectPool.seConnecter();
+			pstmt = cnx.prepareStatement(INSERER);
+			pstmt.setString(1, libelle);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new DalException("Problème - ajouterCategorie" + e.getMessage());
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (cnx != null) {
+					cnx.close();
+				}
+			} catch (SQLException e) {
+				throw new DalException("Problème -fermerConnexion" + e.getMessage());
+			}
+		}
+
+	}
+
+	public static Categorie selectParNumero(int noCategorie) throws DalException {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Categorie categorie = null;
+
+		try {
+			cnx = DBConnectPool.seConnecter();
+			pstmt = cnx.prepareStatement(SELECTBY);
+			pstmt.setInt(1, noCategorie);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				categorie = new Categorie();
+				categorie.setNoCategorie(rs.getInt("no_categorie"));
+				categorie.setLibelle(rs.getString("libelle"));
+			}
+
+		} catch (SQLException e) {
+			throw new DalException("Problème - RechercherUtilisateur" + e.getMessage());
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (cnx != null) {
+					cnx.close();
+				}
+			} catch (SQLException e) {
+				throw new DalException("Problème - fermerConnexion" + e.getMessage());
+			}
+
+		}
+
+		return categorie;
+	}
+
+	public static Categorie selectParId(int noCategorie) throws DalException {
+		Connection cnx = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Categorie categorie = null;
+
+		try {
+			cnx = DBConnectPool.seConnecter();
+			pstmt = cnx.prepareStatement(SELECTBY);
+			pstmt.setInt(1, noCategorie);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				categorie = new Categorie();
+				categorie.setNoCategorie(rs.getInt("no_categorie"));
+				categorie.setLibelle(rs.getString("libelle"));
+			}
+
+		} catch (SQLException e) {
+			throw new DalException("Problème - RechercherUtilisateur" + e.getMessage());
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (cnx != null) {
+					cnx.close();
+				}
+			} catch (SQLException e) {
+				throw new DalException("Problème - fermerConnexion" + e.getMessage());
+			}
+
+		}
+
+		return categorie;
+	}
 
 }
