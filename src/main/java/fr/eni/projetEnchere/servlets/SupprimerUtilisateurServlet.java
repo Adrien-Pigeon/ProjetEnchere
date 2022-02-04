cfbd2f30916c3daef9b6e704d98c16747d12f1c0
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-04 09:36:56 +0100

    CHANGE

--- src/main/java/fr/eni/projetEnchere/servlets/SupprimerUtilisateurServlet.java
+++ src/main/java/fr/eni/projetEnchere/servlets/SupprimerUtilisateurServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class SuppimerUtilisateurServlet
  */
 @WebServlet("/SuppimerUtilisateurServlet")
-public class SuppimerUtilisateurServlet extends HttpServlet {
+public class SupprimerUtilisateurServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SuppimerUtilisateurServlet() {
+    public SupprimerUtilisateurServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
