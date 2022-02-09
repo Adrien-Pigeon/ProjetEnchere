f532dce9caaf803a93cb69d1f51989cb2097cff5
apigeon2021 <apigeon2021@177SE44-9KZ355J.ad.campus-eni.fr>
2022-02-09 10:41:12 +0100

    CHANGE

--- src/main/java/fr/eni/projetEnchere/servlets/ListeEnchereUtilisateurServlet.java
+++ src/main/java/fr/eni/projetEnchere/servlets/ListeEnchereUtilisateurServlet.java
@@ -1,21 +1,21 @@
 package fr.eni.projetEnchere.servlets;
 
 import java.io.IOException;
-import java.util.List;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 
 import fr.eni.projetEnchere.bll.ArticleVenduManager;
-import fr.eni.projetEnchere.bll.CategorieManager;
+
 import fr.eni.projetEnchere.bll.UtilisateurManager;
 import fr.eni.projetEnchere.bll.Exception.BllException;
 import fr.eni.projetEnchere.bo.ArticleVendu;
-import fr.eni.projetEnchere.bo.Categorie;
+
 import fr.eni.projetEnchere.bo.Utilisateur;
 import fr.eni.projetEnchere.dal.Exception.DalException;
 
